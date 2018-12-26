c270860901d2e3ef6386b47746cf61ecdeb39fef
Roman Golubev <gromniki@gmail.com>
2018-12-26 21:32:45 +0300

    make timer v1.2

--- lesson-07/yoga/js/script.js
+++ lesson-07/yoga/js/script.js
@@ -44,7 +44,38 @@ window.addEventListener('DOMContentLoaded', () => {
       let t = Date.parse(endtime) - Date.parse(new Date());  // записываю в переменную разницу даты дэдлайна и настоящей даты в мс
       let seconds = Math.floor((t / 1000) % 60);  // Получаю секунды из мс делением на 1000 и чтобы получить число не больше 59, применяю остаток от деления на 60, так как в минуте 60 сек
       let minutes = Math.floor((t / 1000 / 60) % 60);  // Получаю минуты из мс
+      let hours = Math.floor(t / (1000 * 60 * 60));  // Получаю часы из мс
+      
+      // let hours = Math.floor((t / 1000 / 60 / 60) % 24);  // Второй вариант получения часов
+      // let days = Math.floor(t / (1000 * 60 * 60 * 24));  // Получаю дни из мс
+
+      return {
+        'total': t,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };  // чтобы получить все данные из функции, нужно вернуть объект с нужными значениями
     }
+
+    function setClock(id, endtime) {
+      let timer = document.getElementById(id);
+      let hours = timer.querySelector('.hours');
+      let minutes = timer.querySelector('.minutes');
+      let seconds = timer.querySelector('.seconds');
+      let timeInterval = setInterval(updateClock, 1000);
+
+      function updateClock() {
+        let t = getTimeRemaining(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+      }
+
+    }
+
+    setClock('timer', deadline);
+
+    console.log(Date.parse(new Date()) / 1000 / 60);
 
     //e.addEventListener('click', () => {});  // шаблон
   });
