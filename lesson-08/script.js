11494e4377b5442d40d1ecb918d60e79404a4ec0
Roman Golubev <gromniki@gmail.com>
2019-01-04 01:48:16 +0300

    this v1.4

--- lesson-08/script.js
+++ lesson-08/script.js
@@ -62,9 +62,29 @@ function sayName(surname) {
 }
 
 console.log(sayName.call(user, ''));  // насильно связали функцию с данными объекта user. Можно передать только строку в виде параметра
-console.log(sayName.apply(user));  // второй метод может передать уже массив
+console.log(sayName.apply(user, ['Snow']));  // в случае, если надо передать много параметров с данными
 
+function count(number) {
+    return this * number;
+}
+
+let double = count.bind(2);  // метод bind позволяет прокидывать в функцию count нужное значение и заменяет this
+console.log(double(3));  // 3 это параметр функции count, т.о. получается 2 * 3 = 6
+console.log(double(10));  // 20
+// всё это жёсткие методы привязки контекста. Используются не так часто
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
+    }
+    showThis();  // произойдет вызов window
+});
 
 // 1. Просто вызов функции – window/undefined
 // 2. Метод объекта – this = объект
 // 3. Конструктор (new) – this = новый созданный объект
+// 4. Указание конкретного контекста – call, apply, bind
