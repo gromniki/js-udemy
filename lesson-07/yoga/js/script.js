1f525f67e35173818dbfa12cb2dd09c89e2de5ad
Roman Golubev <gromniki@gmail.com>
2019-01-04 23:58:20 +0300

    modal v1.2

--- lesson-07/yoga/js/script.js
+++ lesson-07/yoga/js/script.js
@@ -106,14 +106,16 @@ window.addEventListener('DOMContentLoaded', () => {
     let overlay = document.querySelector('.overlay');
     let close = document.querySelector('.popup-close');
 
-    more.addEventListener('click', () => {
+    more.addEventListener('click', function() {
         overlay.style.display = 'block';
         this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         overlay.style.display = 'none';
-        
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
     });
 
     //e.addEventListener('click', () => {});  // шаблон
