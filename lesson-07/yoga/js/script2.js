986910d46e0a4c1b354b3d4f230d64fbeed9b027
Roman Golubev <gromniki@gmail.com>
2019-01-14 23:05:17 +0300

    homework assignment 12 v1.1

--- lesson-07/yoga/js/script2.js
+++ lesson-07/yoga/js/script2.js
@@ -13,21 +13,62 @@ window.addEventListener('DOMContentLoaded', function() {
     // hello();
     // выведет undefined
 
-    let names = [
-        'Alex',
-        '',
-        'ludmila',
-        'Viktor',
-        '',
-        'oleg',
-        'iNna',
-        'Ivan',
-        'Alex',
-        'Olga',
-        ' Ann',
-        'aa'
-    ];
+    // let names = [
+    //     'Alex',
+    //     '',
+    //     'ludmila',
+    //     'Viktor',
+    //     '',
+    //     'oleg',
+    //     'iNna',
+    //     'Ivan',
+    //     'Alex',
+    //     'Olga',
+    //     ' Ann',
+    //     'aa'
+    // ];
 
-    console.log(names);
+    // console.log(names);
+
+    
+// Создать класс options
+// Он должен содержать свойства: height, width, bg, fontSize, textAlign
+
+// Он должен содержать метод, создающий новый div на странице,
+// записывающий в него любой текст и при помощи cssText изменять свой стиль из переданных параметров
+
+// Создать новый объект через класс
+
+// Вызвать его метод и получить элемент на странице
+
+    let testDiv = document.querySelector('.more');
+    class Options {
+        constructor(width, height, fontSize, textAlign, bg) {
+            this.width = width;
+            this.height = height;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+            this.bg = bg;
+        }
+
+        createDiv() {
+            let div  = document.createElement('div');
+            //let text = document.createTextNode('Привет земляне!');
+
+            div.textContent = 'Привет земляне!';
+
+            return div;
+
+            //return `<div class="test class options"> Проверка дива + </div>`;
+        }
+    }
+
+    let myText = new Options(40, 40, 24, 'center', '#ff0000');
+
+    console.log(myText.createDiv());
+    //console.log(testDiv);
+
+
+    //document.body.insertBefore(myText.createDiv(), testDiv);
     
 });
