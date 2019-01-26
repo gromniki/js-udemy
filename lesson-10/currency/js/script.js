855579d44214b4f500962d7ba9ac1c8df7ea0736
Roman Golubev <gromniki@gmail.com>
2019-01-26 23:44:32 +0300

    currency script

--- lesson-10/currency/js/script.js
+++ lesson-10/currency/js/script.js
@@ -5,6 +5,8 @@ let inputRub = document.getElementById('rub'),
 inputRub.addEventListener('input', () => {
     let request = new XMLHttpRequest();
 
+    //let promise = new Promise(function(resolve, reject) {});
+
     request.open('GET', 'js/current.json');
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
