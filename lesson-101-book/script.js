6a3795bc4c7fd47625ce93948650829bca86fea8
Roman Golubev <gromniki@gmail.com>
2019-02-03 23:41:33 +0300

    added lesson 101. JavaScript for children

---
+++ lesson-101-book/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    /**
+     * Урок 2. Типы данных
+     */
+    
+    // Сколько секунд в году
+    const SECONDS_IN_A_MINUTE = 60;
+    const MINUTES_IN_AN_HOUR = 60;
+    const SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * MINUTES_IN_AN_HOUR;
+    
+    console.log(SECONDS_IN_AN_HOUR);
+
+    const HOURS_IN_A_DAY = 24;
+    const SECONDS_IN_A_DAY = SECONDS_IN_AN_HOUR * HOURS_IN_A_DAY;
+
+    console.log(SECONDS_IN_A_DAY);
+});
