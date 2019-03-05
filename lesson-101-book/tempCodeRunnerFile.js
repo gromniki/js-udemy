0d07e04ef3ea6535837fdbf414fe3ed13efab047
Roman Golubev <gromniki@gmail.com>
2019-03-05 22:57:23 +0300

    Добавил возраст

---
+++ lesson-101-book/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+    const SECONDS_IN_A_MINUTE = 60;
+    const MINUTES_IN_AN_HOUR = 60;
+    const SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * MINUTES_IN_AN_HOUR;
+    
+    console.log(SECONDS_IN_AN_HOUR);
+
+    const HOURS_IN_A_DAY = 24;
+    const SECONDS_IN_A_DAY = SECONDS_IN_AN_HOUR * HOURS_IN_A_DAY;
+
+    console.log(SECONDS_IN_A_DAY);
+
+    const DAYS_IN_A_YEAR = 365;
+    const SECONDS_IN_A_YEAR = SECONDS_IN_A_DAY * DAYS_IN_A_YEAR;
+
+    console.log(SECONDS_IN_A_YEAR);
+
+    // Какой возраст в секундах
+    let age = 37;
+    age * SECONDS_IN_A_YEAR;
